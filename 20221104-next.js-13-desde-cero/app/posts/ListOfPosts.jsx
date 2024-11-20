a85a333f5043c2f0c40c5a76b047035a41be30f0
Leandro Mendez <leandro.mendez@wearepsh.com>
2024-11-20 00:02:18 -0300

    delete .next folder

--- 20221104-next.js-13-desde-cero/app/posts/ListOfPosts.jsx
+++ 20221104-next.js-13-desde-cero/app/posts/ListOfPosts.jsx
@@ -2,14 +2,15 @@ import Link from 'next/link';
 import { LikeButton } from './LikeButon';
 
 const fetchPosts = () => {
-  console.log('ESTOY FETCHEANDO LOS POSTS!!!!!!!!!!!!!!');
+  console.log('ESTOY FETCHEANDO LOS POSTS!!!!!!!!!!!!!!'); 
+
   return fetch('https://jsonplaceholder.typicode.com/posts', {
     // // esto le indica al browser que no guarde en cache la data. Lo que vuelve no-estatico a este componente.
     // cache: 'no-store',
     // Cada 60 segundos, si ha recibido una request, revalida y vuelve a generar un archivo estatico
     // en caso de haber recibido nuevos datos
     // Es una de las mejores soluciones para fetchear, si no sabes que va a ser estatico el contenido de esta pagina
-    next: {
+    next: {  // incremental static regeneration
       revalidate: 60,
     },
   }).then((res) => res.json());
